89370aecb3be3c788b69db536b99e332feedbaf7
Tomasz <romukkemot@gmail.com>
2021-04-26 12:17:32 +0200

    .

---
+++ src/com/company/Reader.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Reader {
+    public static ArrayList<KartaPlatnicza> getKlienci() {
+        ArrayList<KartaPlatnicza> klienci = new ArrayList<>();
+
+        try {
+            File plik = new File("klienci.csv");
+            Scanner scanner = new Scanner(plik);
+            while(scanner.hasNextLine())
+            {
+                String data = scanner.nextLine();
+
+                String[] clients = data.split(",");
+                String imie = clients[0];
+                String nazwisko = clients[1];
+                long numerKonta = Long.parseLong(clients[2].replaceAll("\\s+", ""));
+                short numerPIN = Short.parseShort(clients[3]);
+                double srodki = Double.parseDouble(clients[4]);
+
+                if ("visa".equals(clients[5].toLowerCase())) {
+                    KartaPlatniczaVisa kpVisa = new KartaPlatniczaVisa(imie, nazwisko, numerKonta, numerPIN, srodki);
+                    klienci.add(kpVisa);
+                } else if ("mastercard".equals(clients[5].toLowerCase())) {
+                    KartaPlatniczaMastercard kpMastercard = new KartaPlatniczaMastercard(
+                            imie, nazwisko, numerKonta, numerPIN, srodki);
+                    klienci.add(kpMastercard);
+                } else {
+                    System.out.println("Nie wspieramy twojej karty!");
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.print("Wystąpił błąd pliku!");
+        }
+
+        return klienci;
+    }
+}
