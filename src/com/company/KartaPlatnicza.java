b1e93234610da340858bf1b087a38ec36def72bc
Tomasz <romukkemot@gmail.com>
2021-04-26 14:05:04 +0200

    .

--- src/com/company/KartaPlatnicza.java
+++ src/com/company/KartaPlatnicza.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class KartaPlatnicza extends Osoba{
     private long numerKarty;
     private short PIN;
@@ -53,15 +51,13 @@ public class KartaPlatnicza extends Osoba{
         System.out.printf("Masz %.2f pieniędzy na koncie!\n", srodki);
     }
 
-    public void wyplacPieniadze(int wyplacanaKwota) {
-        wyswietlBalans();
-
+    public void wyplacPieniadze(double wyplacanaKwota) {
         if (wyplacanaKwota > srodki) {
-            System.out.println("Przekroczyłeś stan konta!");
+            System.out.println("Nie można wypłacić, przekroczyłeś stan konta!");
         } else if (wyplacanaKwota == 0) {
             System.out.println("Nie możesz wypłacić 0zł!");
         } else {
-            System.out.printf("Wypłacono %.2f pieniędzy z konta!\n", srodki);
+            System.out.printf("Wypłacono %.2f pieniędzy z konta!\n", wyplacanaKwota);
             srodki -= wyplacanaKwota;
         }
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -11,39 +10,67 @@ public class Main {
         ArrayList<KartaPlatnicza> klienci = Reader.getKlienci();
         KartaPlatnicza kartaPlatnicza = Reader.getKarta(klienci);
 
+        boolean czyVisa = "visa".equals(kartaPlatnicza.getProducentKarty().toString());
+
+//        lista opcji
         ArrayList<String> opcje = new ArrayList<>();
         opcje.add("Wyświetl środki");
         opcje.add("Wypłać pieniądze");
-//        opcje.add("Wpłać pieniądze");
-
-        System.out.println("Wybierz opcje:");
-        for (int i = 0; i < opcje.size(); i++) {
-            System.out.printf("%d: %s%n", i + 1, opcje.get(i));
+        if (czyVisa) {
+            opcje.add("Wpłać pieniądze");
         }
+        opcje.add("Wyjście");
+
         int wybranaOpcja = -1;
-        try {
-            wybranaOpcja = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Nie ma takiej opcji!");
-        }
+        while (true) {
+//            pobieramy opcje od uzytkownika
+            System.out.println("Wybierz opcje:");
+            for (int i = 0; i < opcje.size(); i++) {
+                System.out.printf("%d: %s%n", i + 1, opcje.get(i));
+            }
+            try {
+                wybranaOpcja = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Nie ma takiej opcji!");
+                continue;
+            }
 
-        switch (wybranaOpcja) {
-            case 1:
-                kartaPlatnicza.wyswietlBalans();
+
+//            sprawdzanie jaka opcja jest wybrana
+            if (!(wybranaOpcja != 4 && czyVisa || wybranaOpcja != 3 && !czyVisa)) {
+                System.out.println("Zamknięto program");
                 break;
-            case 2:
+            }
+
+            if (wybranaOpcja == 1) {
+
+                kartaPlatnicza.wyswietlBalans();
+
+            } else if (wybranaOpcja == 2) {
+
                 try {
-                    kartaPlatnicza.wyplacPieniadze(Integer.parseInt(scanner.nextLine()));
+                    System.out.print("Ile chcesz wypłacić pieniędzy: ");
+                    kartaPlatnicza.wyplacPieniadze(Double.parseDouble(scanner.nextLine()));
                 } catch (NumberFormatException e) {
                     System.out.println("Błędny format danych!");
                 }
-                break;
-            case 3:
-//                ...
-                break;
-            default:
+
+            } else if (wybranaOpcja == 3 && czyVisa) {
+
+                try {
+                    System.out.print("Ile chcesz wpłacic pieniędzy: ");
+                    ((KartaPlatniczaVisa) kartaPlatnicza).doliczSrodki(Double.parseDouble(scanner.nextLine()));
+                } catch (NumberFormatException e) {
+                    System.out.println("Błędny format danych!");
+                }
+
+            } else {
+
                 System.out.println("Nie ma takiej opcji!");
+
+            }
         }
+
 
         scanner.close();
     }
